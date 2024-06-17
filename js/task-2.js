e011ac08fb5a058a3c035b0c232b265db672920f
Tetiana <shchepanovat@gmail.com>
2024-06-17 19:29:52 +0300

    update all

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee){
     const totalPrice= price + deliveryFee;
-    const message = `You ordered ${country} droids worth ${totalPrice} credits`;
+    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
     return message;
     
 }   
